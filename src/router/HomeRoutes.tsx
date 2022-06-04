29f74cffd33a22b5618d6434fde87bab883850c8
honda <hondasan1981@gmail.com>
2022-06-04 17:56:53 +0900

    ルーティング修正

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -3,21 +3,19 @@ import { Page404 } from "../components/pages/Page404";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
 
-const homeUrl = process.env.PUBLIC_URL;
-
 export const homeRoutes = [
     {
-        path:homeUrl,
+        path:"/",
         exact : true,
         children: <Home />
     },
     {
-        path:homeUrl + "/user_management",
+        path:"/user_management",
         exact : false,
         children: <UserManagement />
     },
     {
-        path:homeUrl + "/setting",
+        path:"/setting",
         exact : false,
         children: <Setting />
     },
